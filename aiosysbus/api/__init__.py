6b4a846a1b6397f564839e1d281f9c5f3bbd369b
cyr-ius <cyr-ius@ipocus.net>
2019-12-17 11:23:22 +0100

    Simple init class

--- aiosysbus/api/__init__.py
+++ aiosysbus/api/__init__.py
@@ -1,3 +1,11 @@
 #-*- coding:utf-8 -*-
 
 __all__ = []
+
+from aiosysbus.api.call import Call
+from aiosysbus.api.connection import Connection
+from aiosysbus.api.dhcp import Dhcp
+from aiosysbus.api.nat import Nat
+from aiosysbus.api.screen import Screen
+from aiosysbus.api.system import System
+from aiosysbus.api.wifi import Wifi
