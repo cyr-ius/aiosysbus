56a4628587c317ac047ad5662cf5b8e649f7c21c
cyr-ius <cyr-ius@ipocus.net>
2019-12-17 11:23:06 +0100

    Fix Screen API

--- aiosysbus/api/screen.py
+++ aiosysbus/api/screen.py
@@ -3,6 +3,6 @@ class Screen:
     def __init__(self, access):
         self._access = access
 
-    def get_screen_ShowWifiPassword(self):
+    async def set_screen_ShowWifiPassword(self):
         ''' Show wifi password (display on box) '''
         return await self._access.post('Screen:getShowWifiPassword')
