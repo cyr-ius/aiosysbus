0ab9b798af6559033ff2557a83ad58a74458fe05
cyr-ius <cyr-ius@ipocus.net>
2019-12-17 11:24:51 +0100

    Bump version 1.3

--- aiosysbus/__init__.py
+++ aiosysbus/__init__.py
@@ -4,7 +4,7 @@
 Provides authentification and row access to Livebox.
 '''
 name='aiosysbus'
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 __all__ = ['aiosysbus']
 
-from aiosysbus.aiosysbus import Sysbus
+from .aiosysbus import Sysbus
