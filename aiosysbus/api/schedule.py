d51237a7f62c0576bc92361602ed3dd3d0273a73
cyr-ius <cyr-ius@ipocus.net>
2021-05-25 19:05:55 +0200

    Add schedule class

--- aiosysbus/api/schedule.py
+++ aiosysbus/api/schedule.py
@@ -1,5 +1,7 @@
+"""Scheduler for Internet Access."""
+
 class Schedule:
-    """Scheduler for device."""
+    """Scheduler class."""
 
     def __init__(self, access):
         """Init."""
@@ -12,7 +14,7 @@ class Schedule:
         """
         return self._access("Scheduler", "getSchedule", conf)
 
-    def add_schedule(self, conf=None):
+    def add_schedule(self, conf):
         """Add schedule.
 
         Enable MAC Address
@@ -23,3 +25,35 @@ class Schedule:
         conf = {"parameters":{"type":"ToD","info":{"ID":"01:02:03:04:05:06","enable":true,"base":"Weekly","def":"Enable","override":"","value":"Enable","schedule":[{"begin":0,"end":3600,"state":"Disable"},{"begin":601200,"end":604800,"state":"Disable"}]}}}
         """
         return self._access("Scheduler", "addSchedule", conf)
+
+    def enable_schedule(self, conf):
+        """Get schedule information."""
+        return self._access("Scheduler", "enableSchedule", conf)
+
+    def set_schedule(self, conf):
+        """Get schedule information."""
+        return self._access("Scheduler", "overridedSchedule", conf)
+
+    def set_state(self, conf):
+        """Get schedule information."""
+        return self._access("Scheduler", "overrideState", conf)
+
+    def remove_schedule(self, conf):
+        """Get schedule information."""
+        return self._access("Scheduler", "removeSchedule", conf)
+
+    def get_scheduletypes(self, conf=None):
+        """Get schedule information."""
+        return self._access("Scheduler", "getScheduleTypes", conf)
+
+    def get_schedules(self, conf=None):
+        """Get schedule information."""
+        return self._access("Scheduler", "getSchedules", conf)
+
+    def get_completeschedules(self, conf=None):
+        """Get schedule information."""
+        return self._access("Scheduler", "getCompleteSchedules", conf)
+
+    def set_wlanscheduler_state(self, conf=None):
+        """Get schedule information."""
+        return self._access("WLanScheduler", "setState", conf)
